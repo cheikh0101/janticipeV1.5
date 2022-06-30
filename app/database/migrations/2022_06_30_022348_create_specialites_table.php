2ae3dc961124a610c9979fa01bc9916ee1c1e74e
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-06-30 02:57:12 +0000

    backpack crud and add some tables

---
+++ app/database/migrations/2022_06_30_022348_create_specialites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialites', function (Blueprint $table) {
+            $table->id();
+            $table->string('libelle')->unique();
+            $table->string('code')->unique();
+            $table->boolean('enabled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialites');
+    }
+}
