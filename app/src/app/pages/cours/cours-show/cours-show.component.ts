02501b6a50c7134b99d0b9ca0bbe3a590f7cb66a
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-08-30 09:15:28 +0000

    https://janticipe.atlassian.net/browse/JV2-30

--- app/src/app/pages/cours/cours-show/cours-show.component.ts
+++ app/src/app/pages/cours/cours-show/cours-show.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Cours } from 'src/app/model/Cours';
 import { Type } from 'src/app/model/Type';
-import { BaseService } from 'src/app/service/base.service';
 import { CoursService } from 'src/app/service/cours.service';
 import { IndexService } from 'src/app/service/index.service';
 
@@ -30,8 +29,6 @@ export class CoursShowComponent implements OnInit {
     this.coursSrv.findOneById(this.activatedRoute.snapshot.params["id"])
       .then((data: any) => {
         this.cours = data;
-        console.log(this.cours);
-
       })
       .catch(() => { })
   }
@@ -40,8 +37,6 @@ export class CoursShowComponent implements OnInit {
     this.indexSrv.getTypes()
       .then((data:any) => {
         this.types = data;
-        console.log(this.types);
-
       } )
   }
 }
